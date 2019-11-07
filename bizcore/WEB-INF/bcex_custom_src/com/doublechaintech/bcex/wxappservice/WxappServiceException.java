e5095385e2a26429f9654dbd0e56630b32b1d7be
Clariones <clariones@163.com>
2019-11-07 19:01:23 +0800

    setup baseline

--- bizcore/WEB-INF/bcex_custom_src/com/doublechaintech/bcex/wxappservice/WxappServiceException.java
+++ bizcore/WEB-INF/bcex_custom_src/com/doublechaintech/bcex/wxappservice/WxappServiceException.java
@@ -9,32 +9,26 @@ public class WxappServiceException extends BcexException {
 
 	public WxappServiceException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public WxappServiceException(List<Message> messageList) {
 		super(messageList);
-		// TODO Auto-generated constructor stub
 	}
 
 	public WxappServiceException(Message message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public WxappServiceException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public WxappServiceException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public WxappServiceException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
